bc9ccf041e1256d68d9770d31afe8c7744e80542
ashwinbawne <126654501+ashwinbawne@users.noreply.github.com>
2023-10-31 21:57:43 +0530

    Add files via upload

---
+++ GlobalVariable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace WebMVC
+{
+    public static class GlobalVariable
+    {
+        public static HttpClient APIProject = new HttpClient();
+
+       
+    }
+}
